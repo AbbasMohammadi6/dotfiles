984d4bd2a12848674cdf945604056d08e6ca346f
Abbas <abbasmohammadi@gmail.com>
2022-11-11 18:00:52 +0330

    use telescop-file-browser as file-tree

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -164,11 +164,6 @@ _G.packer_plugins = {
     path = "/home/abbas/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
-  ["nvim-tree.lua"] = {
-    loaded = true,
-    path = "/home/abbas/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
-    url = "https://github.com/kyazdani42/nvim-tree.lua"
-  },
   ["nvim-treesitter"] = {
     loaded = true,
     path = "/home/abbas/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
@@ -203,6 +198,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/abbas/.local/share/nvim/site/pack/packer/start/popup.nvim",
     url = "https://github.com/nvim-lua/popup.nvim"
+  },
+  ["telescope-file-browser.nvim"] = {
+    loaded = true,
+    path = "/home/abbas/.local/share/nvim/site/pack/packer/start/telescope-file-browser.nvim",
+    url = "https://github.com/nvim-telescope/telescope-file-browser.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
