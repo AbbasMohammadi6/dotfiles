895b67d60dedb186b3ec5950aad2f62aac8f6155
Abbas <abbasmohammadi@gmail.com>
2023-01-08 22:43:39 +0330

    add prettier configs to null-ls, so we don't have to copy prettierrc to all projects

--- nvim/lua/abbas/lsp/null-ls.lua
+++ nvim/lua/abbas/lsp/null-ls.lua
@@ -6,15 +6,25 @@ end
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-local diagnostics = null_ls.builtins.diagnostics
+-- local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
 	debug = false,
 	sources = {
-		-- formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		formatting.prettier,
+		formatting.prettier.with({
+			extra_args = {
+				"--print-width 120",
+				"--trailing-comma es5",
+				"--tab-width 2",
+				"--use-tabs",
+				"--single-quote",
+				"--jsx-single-quote",
+				"--arrow-parens avoid",
+			},
+		}),
+		-- formatting.prettier,
 		-- formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
-    -- diagnostics.flake8
+		-- diagnostics.flake8
 	},
 })
