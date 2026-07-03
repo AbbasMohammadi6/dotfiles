8f26d1efc652a2aeee2a3630f5fda20c1e211e75
Ab.mohammadi <ab.mohammadi@sadad.co.ir>
2026-07-03 11:06:10 +0330

    remove useless comments

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -26,32 +26,12 @@ return {
     --
     -- See :h blink-cmp-config-keymap for defining your own keymap
     keymap = {
-      -- preset = 'enter',
       ['<Tab>'] = { 'select_next', 'fallback' },
       ['<S-Tab>'] = { 'select_prev', 'fallback' },
       ['<CR>'] = { 'accept', 'fallback' },
       ['<Right>'] = { 'snippet_forward', 'fallback' },
       ['<Left>'] = { 'snippet_backward', 'fallback' },
     },
-
-    -- >lua
-    --     ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-    --     ['<C-e>'] = { 'hide', 'fallback' },
-    --     ['<CR>'] = { 'accept', 'fallback' },
-    --
-    --     ['<Tab>'] = { 'snippet_forward', 'fallback' },
-    --     ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
-    --
-    --     ['<Up>'] = { 'select_prev', 'fallback' },
-    --     ['<Down>'] = { 'select_next', 'fallback' },
-    --     ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
-    --     ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
-    --
-    --     ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
-    --     ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
-    --
-    --     ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
-    -- <
 
     appearance = {
       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
