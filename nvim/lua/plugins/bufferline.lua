ac634b88e9347e2857fe3876f3fd0b7f426ed4c3
Ab.mohammadi <ab.mohammadi@sadad.co.ir>
2026-07-03 11:05:53 +0330

    fix between buffers movment

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -3,14 +3,16 @@ return {
   version = "*",
   dependencies = "nvim-tree/nvim-web-devicons",
   keys = {
-    { "<leader>h", "BufferLineCyclePrev<cr>",      desc = "Prev Buffer" },
+    { "<leader>h", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
     { "<leader>l", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
     { "<leader>;", "<cmd>bd<cr>",                  desc = "Close Buffer" },
   },
   opts = {
-    mode = "buffers", -- Shows open files as tabs
-    diagnostics = "nvim_lsp",
-    show_buffer_close_icons = true,
-    show_close_icon = false,
+    options = {
+      mode = "buffers",
+      diagnostics = "nvim_lsp",
+      show_buffer_close_icons = true,
+      show_close_icon = false,
+    }
   },
 }
